c6adbbf39b604488caac3679653e8fb0a68a659a
Scriptman 777 <dave56938@gmail.com>
2021-03-27 15:51:23 +0100

    Added data rendering, gradient and description to water.py. Added two more sets of data

--- BCProject/Water/water.py
+++ BCProject/Water/water.py
@@ -1,25 +1,63 @@
 import numpy as np
 import moderngl
 import imgui
+import math
 from UI.window import Window
 from moderngl_window.integrations.imgui import ModernglWindowRenderer
 
-def gridInit(size, steps):
+def gridInit(size, steps_x, steps_y):
     #Vertical Lines
-    u = np.linspace(-size, size, steps)
+    u = np.linspace(-size, size, steps_x)
     u = np.array(list(zip(u,u))).flatten()
 
-    v = np.repeat(size,steps)
-    neg_v = np.repeat(-size,steps)
+    v = np.repeat(size,steps_x)
+    neg_v = np.repeat(-size,steps_x)
     v = np.array(list(zip(v,neg_v))).flatten()
        
     out = np.array(list(zip(u,v))).flatten()
 
     #Horizontal lines
 
-    out = np.append(out,np.array(list(zip(v,u))).flatten())
+    u_y = np.linspace(-size, size, steps_y)
+    u_y = np.array(list(zip(u_y,u_y))).flatten()
+
+    v_y = np.repeat(size,steps_y)
+    neg_v_y = np.repeat(-size,steps_y)
+    v_y = np.array(list(zip(v_y,neg_v_y))).flatten()
+
+    out = np.append(out,np.array(list(zip(v_y,u_y))).flatten())
 
     return out
+
+def dataInit(data):
+
+    # Read only flow 
+    heights = []
+    for x in data:
+        num = float(x[2].replace(",","."))
+        heights.append(num)
+
+    # Find maximum flow 
+    max_flow = math.ceil(max(heights))
+
+    norm_heights = []
+    for x in heights:
+        norm = round(x/max_flow,2)
+        norm_heights.append(norm)
+
+    # Create points for the graph
+    step = 1.6/(len(heights)-1)
+    out = np.empty(0)
+
+    for x in range(len(heights)):
+        out = np.append(out,np.array([round(-0.8+x*step,2),-0.8+norm_heights[x]]))
+        out = np.append(out,np.array([round(-0.8+x*step,2),-0.8]))
+
+    return out
+
+
+
+
 
 class Water(Window):
     title = "Water"
@@ -47,14 +85,21 @@ class Water(Window):
             fragment_shader='''
                 #version 330
 
+                in vec4 gl_FragCoord;
+
                 out vec4 outColor;
 
                 uniform bool graph;
                 
                 void main() {
+
+
                     if (graph) 
                     {
-                        outColor = vec4(0.0,0.0,1.0,1.0);   
+                        vec2 res = vec2(1280,720);
+                        vec2 coord = (gl_FragCoord.xy/res)-0.1;
+                        
+                        outColor = vec4(coord.y,coord.y,1.0,1.0);   
                     }
                     else
                     {
@@ -68,27 +113,28 @@ class Water(Window):
             '''
         )
 
-        vertices = np.array([
-            -0.8, 0.8, 
-            -0.8, -0.8, 
-            -0.7, 0.7,
-            -0.7, -0.8, 
-            -0.6, 0.75,
-            -0.6, -0.8,
-        ], dtype='f4')
+
+        # Read data from file
+        f = open("data/Water_data3.txt", "r")
+        line = 0
+        data = []
+        for x in f:
+          data.append(x.split("\t"))
+
 
         self.graph = self.prog['graph']
 
         
         
 
-        self.vbo_grid = self.ctx.buffer(gridInit(0.8,20).astype('f4'))
+        self.vbo_grid = self.ctx.buffer(gridInit(0.8,len(data),20).astype('f4'))
         self.vao_grid = self.ctx.simple_vertex_array(self.prog, self.vbo_grid, 'in_vert')
 
-        self.vbo_graph = self.ctx.buffer(vertices)   
+        self.vbo_graph = self.ctx.buffer(dataInit(data).astype('f4'))   
         self.vao_graph = self.ctx.simple_vertex_array(self.prog, self.vbo_graph, 'in_vert')
 
     def render(self, time: float, frame_time: float):
+
 
 
         back = (1.0, 1.0, 1.0)
@@ -105,7 +151,10 @@ class Water(Window):
 
 
         imgui.begin("Description - Water levels", False)
-        imgui.text("Štítarský potok")
+        imgui.text("This graph displays the streamflow of a river")
+        imgui.text("in cubic meters per second")
+        imgui.text("Source of data:")
+        imgui.text("http://www.pla.cz/portal/sap/")
         imgui.end()
 
         imgui.render()
