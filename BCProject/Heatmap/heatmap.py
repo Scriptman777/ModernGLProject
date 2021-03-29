9828a613815c3f987f201f5b087252371a3fcdf8
Scriptman 777 <dave56938@gmail.com>
2021-03-29 15:39:45 +0200

    Ability to change function added in heatmap.py

--- BCProject/Heatmap/heatmap.py
+++ BCProject/Heatmap/heatmap.py
@@ -2,6 +2,7 @@
 import numpy as np
 import moderngl
 import imgui
+import math
 from pyrr import Matrix44, Quaternion, Vector3, vector
 from UI.window import Window
 from util.camera import Camera
@@ -24,25 +25,30 @@ class Heatmap(Window):
                 #version 330
                 in vec3 vert;
 
-                out float origZ;
+                out float gradient;
 
                 uniform mat4 Mvp;
 
                 void main() {
-                    gl_PointSize = 5;
-                    origZ = vert.z;
+                    gl_PointSize = 3;
+                    gradient = (vert.z+1)*0.5;
                     gl_Position = Mvp * vec4(vert, 1.0);
                 }
             ''',
             fragment_shader='''
                 #version 330
 
-                in float origZ;
+                in float gradient;
 
-                out vec4 color;
+                out vec4 outColor;
+
                 void main() {
 
-                    color = vec4(origZ, origZ, 1.0, 0.5);
+                    vec3 colorA = vec3(1.0,0.1,0.1);
+                    vec3 colorB = vec3(0.0,0.0,1.0);
+
+                    vec3 color = mix(colorA,colorB,gradient);
+                    outColor = vec4(color, 1.0);
                 }
             ''',
         )
@@ -57,31 +63,45 @@ class Heatmap(Window):
 
         self.mvp = self.prog['Mvp']
 
-        self.vbo = self.ctx.buffer(self.initData().astype('f4'))
+        self.vbo = self.ctx.buffer(self.initData(1).astype('f4'))
         self.vao = self.ctx.simple_vertex_array(self.prog, self.vbo, 'vert')
 
         
         
-    def initData(self):
+    def initData(self,function):
         x = np.linspace(-8, 8, 200)
         y = np.linspace(-8, 8, 200)
         out = []
 
         for i in range(len(x)):
             for j in range(len(y)):
-                out = np.append(out,[x[i],y[j],np.sin(np.sqrt(x[i] ** 2 + y[j] ** 2))])
+                out = np.append(out,[x[i],y[j],self.calcFunc(x[i],y[j],function)])
 
         return out
+
+
+    def calcFunc(self,x,y,func):
+        if func == 1:
+            return math.sin(math.sqrt(x ** 2 + y ** 2))
+        if func == 2:
+            return math.cos(x)*math.sin(y)
+        if func == 3:
+            return np.sin(np.sqrt(x ** 2 + y ** 2))
+        if func == 4:
+            return np.sin(np.sqrt(x ** 2 + y ** 2))
 
 
 
     def render(self, time: float, frame_time: float):
 
+        # Camera animation
         self.camera.move_forward()
         self.camera.rotate_left()
         self.camera.move_backwards()
 
         self.mvp.write((self.camera.mat_projection * self.camera.mat_lookat).astype('f4'))
+
+        self.ctx.enable(moderngl.DEPTH_TEST)
 
         self.ctx.enable_only(moderngl.PROGRAM_POINT_SIZE)
         back = (0.2, 0.2, 0.2)
@@ -126,8 +146,18 @@ class Heatmap(Window):
 
     # Events to interact with the visualisation
     def key_event(self, key, action, modifiers):
-        print(key)
-
+        if key == 49 and action == self.wnd.keys.ACTION_PRESS:
+            self.vbo = self.ctx.buffer(self.initData(1).astype('f4'))
+            self.vao = self.ctx.simple_vertex_array(self.prog, self.vbo, 'vert')
+        if key == 50 and action == self.wnd.keys.ACTION_PRESS:
+            self.vbo = self.ctx.buffer(self.initData(2).astype('f4'))
+            self.vao = self.ctx.simple_vertex_array(self.prog, self.vbo, 'vert')
+        if key == 51 and action == self.wnd.keys.ACTION_PRESS:
+            self.vbo = self.ctx.buffer(self.initData(3).astype('f4'))
+            self.vao = self.ctx.simple_vertex_array(self.prog, self.vbo, 'vert')
+        if key == 52 and action == self.wnd.keys.ACTION_PRESS:
+            self.vbo = self.ctx.buffer(self.initData(4).astype('f4'))
+            self.vao = self.ctx.simple_vertex_array(self.prog, self.vbo, 'vert')
 
 
 
