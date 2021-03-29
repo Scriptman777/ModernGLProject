a676130292059cbb1961321ee321bd9adbbb89eb
Scriptman 777 <dave56938@gmail.com>
2021-03-29 13:30:31 +0200

    Added camera from examples, created point rendering and coloring in heatmap.py

---
+++ BCProject/Heatmap/heatmap.py
@@ -0,0 +1,125 @@
+
+import numpy as np
+import moderngl
+import imgui
+from UI.window import Window
+from util.camera import Camera
+from moderngl_window.integrations.imgui import ModernglWindowRenderer
+
+class Heatmap(Window):
+    title = "Title"
+    gl_version = (3, 3)
+
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+
+        imgui.create_context()
+        self.imgui = ModernglWindowRenderer(self.wnd)
+
+        self.prog = self.ctx.program(
+            vertex_shader='''
+                #version 330
+                in vec3 vert;
+
+                out float origZ;
+
+                uniform mat4 Mvp;
+
+                void main() {
+                    gl_PointSize = 2;
+                    origZ = vert.z;
+                    gl_Position = Mvp * vec4(vert, 1.0);
+                }
+            ''',
+            fragment_shader='''
+                #version 330
+
+                in float origZ;
+
+                out vec4 color;
+                void main() {
+
+                    color = vec4(origZ, origZ, 1.0, 1.0);
+                }
+            ''',
+        )
+
+        self.camera = Camera(self.aspect_ratio)
+        self.mvp = self.prog['Mvp']
+
+        self.vbo = self.ctx.buffer(self.initData().astype('f4'))
+        self.vao = self.ctx.simple_vertex_array(self.prog, self.vbo, 'vert')
+
+        
+        
+    def initData(self):
+        x = np.linspace(-8, 8, 200)
+        y = np.linspace(-8, 8, 200)
+        z = []
+
+        for i in range(len(x)):
+            for j in range(len(y)):
+                z = np.append(z,[x[i],y[j],np.sin(np.sqrt(x[i] ** 2 + y[j] ** 2))])
+
+        return z
+
+
+
+    def render(self, time: float, frame_time: float):
+
+        self.mvp.write((self.camera.mat_projection * self.camera.mat_lookat).astype('f4'))
+
+        self.ctx.enable_only(moderngl.PROGRAM_POINT_SIZE)
+        back = (0.2, 0.2, 0.2)
+        self.ctx.clear(back[0],back[1],back[2])
+        self.vao.render(mode=moderngl.POINTS)
+
+        self.render_ui()
+
+    def render_ui(self):
+        imgui.new_frame()
+
+
+        imgui.begin("Description - aaa", False)
+        imgui.text("Lorem ipsum")
+        imgui.end()
+
+
+        imgui.begin("Controls - aaa", False)
+        imgui.text("Press A/D to dolor sit amet")
+        imgui.end()
+
+
+
+        imgui.render()
+        self.imgui.render(imgui.get_draw_data())
+
+    # Events for imgui
+    def mouse_position_event(self, x, y, dx, dy):
+        self.imgui.mouse_position_event(x, y, dx, dy)
+
+    def mouse_drag_event(self, x, y, dx, dy):
+        self.imgui.mouse_drag_event(x, y, dx, dy)
+
+    def mouse_scroll_event(self, x_offset, y_offset):
+        self.imgui.mouse_scroll_event(x_offset, y_offset)
+
+    def mouse_press_event(self, x, y, button):
+        self.imgui.mouse_press_event(x, y, button)
+
+    def mouse_release_event(self, x: int, y: int, button: int):
+        self.imgui.mouse_release_event(x, y, button)
+
+    # Events to interact with the visualisation
+    def key_event(self, key, action, modifiers):
+        print(key)
+
+
+
+
+if __name__ == '__main__':
+    Heatmap.run()
+
+
